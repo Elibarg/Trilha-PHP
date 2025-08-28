7dff09e457289cb366f2a309e593193b66243558
Elibarg <calagabriel31@gmail.com>
2025-08-27 21:22:15 -0300

    re

---
+++ OT1_Gabriel/at3.php
@@ -0,0 +1,7 @@
+<?php
+$nota1 = 4;
+$nota2 = 4;
+$nota3 = 4;
+$resultado = $nota1 + $nota2 + $nota3 /3;
+echo"$resultado";
+?>
---
+++ at5.php
@@ -0,0 +1,11 @@
+<?php
+  $array = array(
+        "cor1" => "verde",
+        "cor2" => "rosa",
+        "cor3" => "preto",
+    );
+    foreach ($array as $key => $value) {
+        echo "Key: $key => Value: $value\n";
+    }
+?>
+

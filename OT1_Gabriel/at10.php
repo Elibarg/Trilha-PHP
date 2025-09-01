5085be36197ddb6452f016496549382225acdeef
Elibarg <calagabriel31@gmail.com>
2025-09-01 19:47:17 -0300

    re

---
+++ OT1_Gabriel/at10.php
@@ -0,0 +1,12 @@
+<?php
+$num1 = 4;
+$num2 = 2;
+
+function calculo($num1,$num2) {
+ $resultado = $num1 * $num2;
+ return $resultado;
+};
+
+$a = calculo($num1,$num2);
+echo "$a";
+?>
---
+++ OT1_Gabriel/at9.php
@@ -0,0 +1,10 @@
+<?php
+$num =-1;
+if ($num >0) {
+    echo "a";
+}  else if ($num <0) {
+    echo "b";
+}else {
+ echo "c";
+}
+?>

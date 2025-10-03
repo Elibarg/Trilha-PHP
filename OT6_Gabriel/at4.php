228b1db352a4bc58ec65d51e0b536946fc991b02
Elibarg <calagabriel31@gmail.com>
2025-10-02 21:36:10 -0300

    l

--- OT6_Gabriel/at4.php
+++ OT6_Gabriel/at4.php
@@ -1,3 +1,17 @@
 <?php
 $conexao = new mysqli("localhost","usuario","senha","banco_de_dados");
 if ($conexao->connect_error) die("falha na conexão: ".$conexao->connect_error);
+
+$email = "usuario@exemplo.com";
+$token = bin2hex(random_bytes(16));
+
+$stmt = $conexao->prepare("UPDATE usuarios SET token_recuperacao = ? WHERE email = ?");
+$stmt->bind_param("ss",$token, $email);
+$stmt->execute();
+
+if ($stmt->affected_rows > 0) {
+    echo "token criado";
+}else{
+    echo "e-mail não encontrado";
+};
+$conexao-> close();
--- OT6_Gabriel/at5.php
+++ OT6_Gabriel/at5.php
@@ -1,3 +1,17 @@
 <?php
 $conexao = new mysqli("localhost","usuario","senha","banco_de_dados");
 if ($conexao->connect_error) die("falha na conexão: ".$conexao->connect_error);
+
+$dados = [
+    ["ana",25],
+    ["Carlos",32],
+    ["Beatriz",29]
+];
+
+$stmt = $conexao->prepare("INSERT INT 0 usuarios (nome, idade) VALUES (?,?)");
+foreach ($idade as $linha){
+    $stmt->bind_param("si",$linha[0],$linha[1]);
+}
+$stmt->execute();
+echo"Múltiplos registros inseridos!";
+$conexao-> close();

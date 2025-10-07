aa72a9e6e69e5e1c61430b4fa0fb90ecad940de1
Elibarg <calagabriel31@gmail.com>
2025-10-06 21:50:43 -0300

    re

--- OT6_Gabriel/at6.php
+++ OT6_Gabriel/at6.php
@@ -1,3 +1,16 @@
 <?php
-$conexao = new mysqli("localhost","usuario","senha","banco_de_dados");
-if ($conexao->connect_error) die("falha na conexão: ".$conexao->connect_error);
+$conexao = new mysqli("localhost", "usuario", "senha", "banco_de_dados");
+if ($conexao->connect_error) die("Falha na conexão: " . $conexao->connect_error);
+
+$sql = "SELECT u.nome, p.produto 
+        FROM usuarios u 
+        JOIN pedidos p ON u.id = p.usuario_id";
+$result = $conexao->query($sql);
+
+echo "<table border='1'><tr><th>Nome</th><th>Produto</th></tr>";
+while ($row = $result->fetch_assoc()) {
+    echo "<tr><td>{$row['nome']}</td><td>{$row['produto']}</td></tr>";
+}
+echo "</table>";
+$conexao->close();
+?>
